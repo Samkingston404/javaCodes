05c3b6367a2204171044f23dc54ff4ad6b97c55d
Samkingston404 <rsamuelvivek900@gamil.com>
2024-10-31 12:58:30 +0530

    aDDED ODD FUNCTION

---
+++ src/Odd.java
@@ -0,0 +1,11 @@
+public class Odd {
+    public static void main(String args[]) {
+        int number = 12;
+        if(number % 2 != 0) {
+            System.out.println("Odd");
+        }
+        else {
+            System.out.println("Even");
+        }
+    }
+}
